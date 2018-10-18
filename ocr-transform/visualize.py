93383252d46245bfc68eb063e86f5382d4bf7ced
Zhang Yungui <rhcad@hotmail.com>
2018-10-18 18:02:21 +0800

    Show result existence

--- ocr-transform/visualize.py
+++ ocr-transform/visualize.py
@@ -10,17 +10,18 @@ from file_util import load_json
 class MainHandler(RequestHandler):
     def get(self):
         names = sorted([f[:-4] for f in listdir('cut-result') if f.endswith('.txt')])
-        self.write('<ol>%s</ol>' % ''.join(['<li><a href="/{0}">{0}</a></li>'.format(n) for n in names]))
+        exists = [load_json(path.join('char-pos', *s.split('_')[:-1], s + '.json')) for s in names]
+        items = ['<li><a href="/{0}">{0}</a>{1}</li>'.format(n, '' if e else ' ?') for n, e in zip(names, exists)]
+        items = [s for s in items if '?' not in s] + [s for s in items if '?' in s]
+        self.write('<ol>%s</ol>' % ''.join(items))
 
 
 class CutHandler(RequestHandler):
     def get(self, name):
         cut = load_json(path.join('char-pos', *name.split('_')[:-1], name + '.json'))
-        blocks = load_json(path.join('block-pos', *name.split('_')[:-1], name + '.json'))
-        if not cut or not blocks:
+        if not cut:
             return self.write(name + ' not exist')
-        blocks = blocks['blocks']
-        self.render('visualize.html', cut=cut, blocks=blocks)
+        self.render('visualize.html', cut=cut)
 
 
 if __name__ == '__main__':
